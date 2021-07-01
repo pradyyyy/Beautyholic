2342688fbdde14ce97f42b891e7c3641f3ad3dde
churroo <pradyaanti@gmail.com>
2021-07-01 11:40:34 +0700

    first commit

---
+++ app/src/main/java/com/pradyanti_1313617004/beautyholic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pradyanti_1313617004.beautyholic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
